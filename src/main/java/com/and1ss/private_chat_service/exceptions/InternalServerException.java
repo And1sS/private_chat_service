d53999126baec1eeab32979931e2f836e63942bb
And1sS <and1sscsgo@gmail.com>
2020-10-09 22:26:06 +0300

    Refactored UserServiceImpl

--- src/main/java/com/and1ss/private_chat_service/exceptions/InternalServerException.java
+++ src/main/java/com/and1ss/private_chat_service/exceptions/InternalServerException.java
@@ -1,3 +1,16 @@
 package com.and1ss.private_chat_service.exceptions;
 
-public class InternalServerException extends RuntimeException {}
+public class InternalServerException extends RuntimeException {
+    private String msg = "";
+
+    public InternalServerException() {}
+
+    public InternalServerException(String msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public String getMessage() {
+        return msg;
+    }
+}
--- src/main/java/com/and1ss/private_chat_service/services/impl/UserServiceImpl.java
+++ src/main/java/com/and1ss/private_chat_service/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.and1ss.private_chat_service.services.impl;
 
+import com.and1ss.private_chat_service.exceptions.BadRequestException;
 import com.and1ss.private_chat_service.exceptions.InternalServerException;
 import com.and1ss.private_chat_service.services.impl.connections.UserServiceConnection;
 import com.and1ss.private_chat_service.api.dto.AccountInfoRetrievalDTO;
@@ -33,10 +34,10 @@ public class UserServiceImpl implements UserService {
         } catch (WebClientResponseException e) {
             switch(e.getRawStatusCode()) {
                 case 401: throw new UnauthorizedException("User is not Authorized");
-                default: throw new InternalServerException();
+                default: throw new InternalServerException(e.getMessage());
             }
         } catch (Exception e) {
-            throw new InternalServerException();
+            throw new InternalServerException(e.getMessage());
         }
 
         return accountInfo;
@@ -44,9 +45,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public AccountInfo findUserById(UUID id) {
-        AccountInfoRetrievalDTO retrievalDTO = userServiceConnection
-                .getUserById(id)
-                .block();
+        AccountInfoRetrievalDTO retrievalDTO;
+        try {
+            retrievalDTO = userServiceConnection
+                    .getUserById(id)
+                    .block();
+        } catch (WebClientResponseException e) {
+            switch (e.getStatusCode()) {
+                case BAD_REQUEST: throw new BadRequestException("Invalid user id");
+                default: throw new InternalServerException(e.getMessage());
+            }
+        } catch (Exception e) {
+            throw new InternalServerException(e.getMessage());
+        }
 
         return AccountInfo.builder()
                 .id(retrievalDTO.getId())
